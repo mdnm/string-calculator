fe566b1ff41076adbb3d30fcb41368d56a63100d
LucasBlasquez <lucasribolli@gmail.com>
2020-11-03 21:37:20 -0300

    convert string to float

--- polishNotation.c
+++ polishNotation.c
@@ -14,79 +14,7 @@ void end()
   exit(0);
 }
 
-// pow para inteiro. pow default so funciona com float
-int int_pow(int base, int exp)
-{
-    int result = 1;
-    while (exp)
-    {
-        if (exp % 2)
-        {
-            result *= base;
-        }
-        exp /= 2;
-        base *= base;
-    }
-    return result;
-}
-
-void expression_with_int()
-{
-    char deitado[TAMANHO][TAMANHO] = {
-        {'1','0'},{'2'},{'3'},{'*'},{'4'},{'-'},{'2'},
-        {'^'},{'4'},{'/'},{'+'},{'6'},{'2'},{'*'},{'+'}
-    };
-
-    char emPe[TAMANHO][TAMANHO];
-
-    int posicaoDeitado = 14; // ja existia
-    int posicaoEmPe = -1;
-    int atual, num1, num2;
-    char oper;
-
-    for(atual = 0; atual <= posicaoDeitado; atual++)
-    {
-        if(isdigit(deitado[atual][0]))
-        {
-            posicaoEmPe++;
-            strcpy(emPe[posicaoEmPe], deitado[atual]);
-        }
-        else
-        {
-            if(posicaoEmPe < 1)
-            {
-                end();
-            }
-            oper = deitado[atual][0];
-            num2 = atoi(emPe[posicaoEmPe]);
-            posicaoEmPe--;
-            num1 = atoi(emPe[posicaoEmPe]);
-
-            switch(oper)
-            {
-            case '+':
-                sprintf(emPe[posicaoEmPe], "%d", num1+num2);
-                break;
-            case '-':
-                sprintf(emPe[posicaoEmPe], "%d", num1-num2);
-                break;
-            case '*':
-                sprintf(emPe[posicaoEmPe], "%d", num1*num2);
-                break;
-            case '/':
-                sprintf(emPe[posicaoEmPe], "%d", num1/num2);
-                break;
-            case '^':
-                sprintf(emPe[posicaoEmPe], "%d", int_pow(num1,num2));
-                break;
-            }
-        }
-    }
-
-    printf("RESULTADO: %s\n", emPe[0]);
-}
-
-void expression_with_float()
+void expression()
 {
     char deitado[TAMANHO][TAMANHO] = {
         {'1','0'},{'2'},{'3'},{'*'},{'4'},{'-'},{'2'},
@@ -115,9 +43,9 @@ void expression_with_float()
                 end();
             }
             oper = deitado[atual][0];
-            num2 = atoi(emPe[posicaoEmPe]);
+            num2 = atof(emPe[posicaoEmPe]);
             posicaoEmPe--;
-            num1 = atoi(emPe[posicaoEmPe]);
+            num1 = atof(emPe[posicaoEmPe]);
 
             switch(oper)
             {
@@ -134,7 +62,7 @@ void expression_with_float()
                 sprintf(emPe[posicaoEmPe], "%f", num1/num2);
                 break;
             case '^':
-                sprintf(emPe[posicaoEmPe], "%f", pow(num1,num2));
+                sprintf(emPe[posicaoEmPe], "%f", pow(num1, num2));
                 break;
             }
         }
@@ -145,8 +73,7 @@ void expression_with_float()
 
 int main()
 {
-    expression_with_float();
-    expression_with_int();
+    expression();
 
     return(0);
 }
